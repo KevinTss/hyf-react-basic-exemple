22cecd6a2743cd1485ef38a7d6539605a459ef09
Kevin <kevin.tassi@gmail.com>
2022-09-26 17:15:40 +0200

    init create reac app minimal

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+const target = document.getElementById('root');
+const root = ReactDOM.createRoot(target);
+root.render(<App />);
